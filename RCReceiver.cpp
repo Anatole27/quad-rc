c50a850d1dcf618937175368a603a916b0d68089
Anatole VERHAEGEN <anatole.verhaegen@gmail.com>
2015-03-13 10:00:44 +0000

    Adding RCReceiver

---
+++ RCReceiver.cpp
@@ -0,0 +1,67 @@
+#include "RCReceiver.h"
+
+RCReceiver::RCReceiver()
+{
+    for(int i = 0;i<6;i++)
+    {
+        pulseLength[i] = 0;
+        pinRead = 0;
+    }
+    pin[0] = PINYAW;
+    pin[1] = PINPITCH;
+    pin[2] = PINROLL;
+    pin[3] = PINTHROTTLE;
+    pin[4] = PINAUTOPILOT;
+    pin[5] = PINAEROBATICS;
+}
+
+void RCReceiver::run()
+{
+    pulseLength[pinRead] = pulseIn(pin[pinRead],HIGH,TIMEOUT);
+    pinRead = (pinRead+1)%6;
+}
+
+float RCReceiver::getYawCommand()
+{
+    float yaw;
+    yaw = YAWMIN + (YAWMAX-YAWMIN)*(float)pulseLength[0]/(PULSEMAX-PULSEMIN);
+    return yaw;
+}
+
+float RCReceiver::getPitchCommand()
+{
+    float pitch;
+    pitch = PITCHMIN + (PITCHMAX-PITCHMIN)*(float)pulseLength[1]/(PULSEMAX-PULSEMIN);
+    return pitch;
+}
+
+float RCReceiver::getRollCommand()
+{
+    float roll;
+    roll = ROLLMIN + (ROLLMAX-ROLLMIN)*(float)pulseLength[2]/(PULSEMAX-PULSEMIN);
+    return roll;
+}
+
+float RCReceiver::getThrottleCommand()
+{
+    float throttle;
+    throttle = THROTTLEMIN + (THROTTLEMAX-THROTTLEMIN)*(float)pulseLength[3]/(PULSEMAX-PULSEMIN);
+    return throttle;
+}
+
+float RCReceiver::getAutopilotEnabled()
+{
+    if(pulseLength[4] > THRESHOLD)
+        return true;
+    else
+        return false;
+}
+
+float RCReceiver::getAerobaticsEnabled()
+{
+    if(pulseLength[5] > THRESHOLD)
+        return true;
+    else
+        return false;
+}
+
